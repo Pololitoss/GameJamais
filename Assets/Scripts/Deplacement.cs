836de5cc4a7bb48a4c462923660dd100b5a1adf6
gabou-02 <gabriel.viard-ramon@epitech.eu>
2026-02-07 14:22:55 +0100

    mvt gauche & droite + jump (infini)

--- Assets/Scripts/Deplacement.cs
+++ Assets/Scripts/Deplacement.cs
@@ -1,16 +1,58 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Deplacement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    private Vector3 velocity = Vector3.zero;
+    private Vector2 moveInput;
+
+    public bool isJumping = false;
+    public float jumpForce;
+
+    void OnEnable()
     {
-        
+        var playerInput = GetComponent<PlayerInput>();
+        if (playerInput != null)
+        {
+            var moveAction = playerInput.actions["Move"];
+            moveAction.performed += OnMove;
+            moveAction.canceled += OnMove;
+            
+            var jumpAction = playerInput.actions["Jump"];
+            jumpAction.performed += OnJump;
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float horizontalMvt = moveInput.x * moveSpeed * Time.deltaTime;
+
+        MovePlayer(horizontalMvt);
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            isJumping = true;
+        }
+    }
+
+    void MovePlayer(float _horizontalMvt){
+        Vector3 targetVelocity = new Vector2(_horizontalMvt, rb.linearVelocity.y);
+        rb.linearVelocity = Vector3.SmoothDamp(rb.linearVelocity, targetVelocity, ref velocity, .05f); //la on applique la vitesse au rb
+
+        if(isJumping == true){
+            rb.AddForce(new Vector2(0f, jumpForce));
+            isJumping = false;
+        }
     }
 }
